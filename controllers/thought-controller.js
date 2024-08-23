7ec8e92847161babb7e6439f97338f170d3f5b26
unknown <bradburr@gmail.com>
2024-08-22 22:03:53 -0600

    fixed an issue with the createThought route

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -31,7 +31,7 @@ module.exports = {
     try {
       const thought = await Thought.create(req.body);
       const user = await User.findOneAndUpdate(
-        { _id: req.body.userId },
+        { username: req.body.username },
         { $addToSet: { thoughts: thought._id } },
         { new: true }
       );
@@ -42,7 +42,7 @@ module.exports = {
         })
       }
 
-      res.json('Created the thought 🎉');
+      res.json('Created a thought 🎉');
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
@@ -72,7 +72,6 @@ module.exports = {
   async deleteThought(req, res) {
     try {
 
-      console.log(req.params.thoughtId)
       const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
 
       if (!thought) {
@@ -84,12 +83,6 @@ module.exports = {
         { $pull: { thoughts: req.params.thoughtId } },
         { new: true }
       );
-
-      if (!user) {
-        return res.status(404).json({
-          message: 'Thought created but no user with this id!',
-        });
-      }
 
       res.json({ message: 'Thought successfully deleted!' });
     } catch (err) {
